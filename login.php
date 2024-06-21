b09990c3cdc5e68e3dd45822443d0658a503df28
Ronilo Rejuso <ronilorejuso21@gmail.com>
2024-06-21 21:31:28 +0800

    fixing bugs for pages signup

--- login.php
+++ login.php
@@ -49,11 +49,6 @@
             <div class="logo">
                 <h1 class="m-0 text-primary" style="color: #ffd698;">Log in</h1>
             </div>
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 927693bf1b5d2809947b51c4257e8d2106397efe
             <form method="post" class="form-horizontal" action="dex.php">
                 <div class="logcontrol-group">
                     <label class="logcontrol-label" for="inputUsername"></label>
@@ -80,37 +75,6 @@
                 </div>
                 <img src="assets/img/our home.png" alt="" style="width: 200px; height: auto; margin-top: 5px;">
             </form>
-=======
-<form method="post" class="form-horizontal" action="dex.php">
-    <div class="logcontrol-group">
-        <label class="logcontrol-label" for="inputUsername"></label>
-        <div class="logcontrols">
-            <input type="text" name="username" id="username" placeholder="Enter Username" maxlength="50" required>
-        </div>
-    </div>
-    <div class="logcontrol-group">
-        <label class="logcontrol-label" for="inputPassword"></label>
-        <div class="logcontrols">
-            <input type="password" name="password" id="password" placeholder="Enter Password" maxlength="10" required>
-            <span toggle="#password" class="field-icon toggle-password">
-                <img src="assets/img/icons/eye1.svg" alt="Toggle Password Visibility">
-            </span>
-        </div>
-        <a href="change_pass.php" style="margin-left: 320px; color: #ffd698;">Forgot Your Password?</a>
-    </div>
-    <div class="logcontrol-group">
-        <div class="logcontrols">
-            <button name="login_btn" type="submit" style="text-align: left;" class="btn btn-info">
-                <span style="float: right; margin-right:10px;">&#9654;</span>
-                <b style="margin-left:15px;"><big>LOG IN</big></b>
-            </button>
-        </div>
-        <a href="signup.php" style="color: #ffd698;"><small>Sign-Up</small></a>
-    </div>
-    <img src="assets/img/our home.png" alt="" style="width: 200px; height: auto; margin-top: 5px; margin-left: 30px;">
-</form>
-
->>>>>>> adec6c4067db50e182594b88c33f3cc3db7b0e54
 
         </div>
     </div>

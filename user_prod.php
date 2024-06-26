75f71b7b83fa280274fb8a01474f70bd2c89c230
RodiRodi <151665346+Rodi2903@users.noreply.github.com>
2024-06-26 18:49:14 +0800

    UI commit

--- user_prod.php
+++ user_prod.php
@@ -83,7 +83,7 @@ include 'user_body1.php';
 </div>
 <div class="floating-navbar">
     <a href="user_index.php"><i class="fas fa-home"></i></a>
-    <a href="user_prod.php"><i class="fas fa-couch"></i></a>
+    <a href="#" class="active"><i class="fas fa-couch"></i></a>
     <a href="user_carts.php"><i class="fas fa-shopping-bag"></i></a>
     <a href="user.php"><i class="fas fa-user"></i></a>
 </div>

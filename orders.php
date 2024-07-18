0e756b01459e888aaec932339a447a8538a11ab0
RodiRodi <151665346+Rodi2903@users.noreply.github.com>
2024-07-18 23:45:46 +0800

    New Commit

--- orders.php
+++ orders.php
@@ -43,7 +43,7 @@ if (!$result) {
     <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects">
     <meta name="author" content="Dreamguys - Bootstrap Admin Template">
     <meta name="robots" content="noindex, nofollow">
-    <title>Our Home</title>
+    <title>FurniView</title>
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/animate.css">
